a4458242767729296cc57165f446cd22f49906f6
kenleyryan-cell <kenley.ryan@students.msswarriors.org>
2026-04-02 19:43:20 +0000

    check

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+let firstName = "animalName";
+let price = height;
+let eligible = isIndoorpet;
+
+docoument.getElementById("string").innerHTML = tpyeof firstName;
+document.getElementById("number").innterHTMl = typeof price; 
+document.getElementById("boolean").innerHTML = typeof eligible;
 
